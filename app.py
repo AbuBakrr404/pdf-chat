476f5e11c72325a6860ecb0844b89941f3f7990e
Abu Bakr <mahomedabubakr84@gmail.com>
2026-05-03 20:00:28 +0200

    remove unused fastembed

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
-from fastembed import TextEmbedding
 from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 import tempfile, os
 
